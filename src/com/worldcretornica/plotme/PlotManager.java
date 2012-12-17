9bd7b58d5f9826da49bf1b4bb2e6de93c1eaf234
GunfighterJ <joseph.jenniges@gmail.com>
2012-12-17 14:40:58 -0600

    Signs lines are 16 characters, so are MC usernames

--- src/com/worldcretornica/plotme/PlotManager.java
+++ src/com/worldcretornica/plotme/PlotManager.java
@@ -381,21 +381,24 @@ public class PlotManager {
 		{
 			sign.setLine(0, "ID:" + id);
 		}
-		if(("Owner:" + plot.owner).length() > 15)
-		{
-			sign.setLine(2, ("Owner:" + plot.owner).substring(0, 15));
-			if(("Owner: " + plot.owner).length() > 30)
-			{
-				sign.setLine(3, ("Owner:" + plot.owner).substring(15, 30));
-			}
-			else
-			{
-				sign.setLine(3, ("Owner:" + plot.owner).substring(15));
-			}
-		}else{
-			sign.setLine(2, "Owner:" + plot.owner);
-			sign.setLine(3, "");
-		}
+                
+                sign.setLine(2,"Owner:");
+                sign.setLine(3, plot.owner);
+//		if(("Owner:" + plot.owner).length() > 15)
+//		{
+//			sign.setLine(2, ("Owner:" + plot.owner).substring(0, 15));
+//			if(("Owner: " + plot.owner).length() > 30)
+//			{
+//				sign.setLine(3, ("Owner:" + plot.owner).substring(15, 30));
+//			}
+//			else
+//			{
+//				sign.setLine(3, ("Owner:" + plot.owner).substring(15));
+//			}
+//		}else{
+//			sign.setLine(2, "Owner:" + plot.owner);
+//			sign.setLine(3, "");
+//		}
 		sign.update(true);
 	}
 	
